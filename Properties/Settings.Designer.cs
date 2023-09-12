b153081fc624ebf9ee1cccd77b7108acaff0da8e
Dzivha Mushanganyisi <dzivha114@gmail.com>
2023-09-12 11:13:53 +0200

    made test database

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -33,5 +33,16 @@ namespace INFOsProject.Properties {
                 return ((string)(this["ProjectDatabaseConnectionString"]));
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\TestDatabase." +
+            "mdf;Integrated Security=True;Connect Timeout=30")]
+        public string TestDatabaseConnectionString {
+            get {
+                return ((string)(this["TestDatabaseConnectionString"]));
+            }
+        }
     }
 }
