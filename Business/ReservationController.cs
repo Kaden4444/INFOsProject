e8a0c0e233d03201d46feb8487b906f68c5e0c8e
Dzivha Mushanganyisi <dzivha114@gmail.com>
2023-10-02 14:39:34 +0200

    "finished" reservationController

--- Business/ReservationController.cs
+++ Business/ReservationController.cs
@@ -1,12 +1,103 @@
-﻿using System;
+﻿using INFOsProject.Data;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace INFOsProject.Business
 {
-    internal class ReservationController
+    public class ReservationController
     {
+
+        #region Data Members
+        ReservationDB ReservationDB;
+        Collection<Reservation> Reservations;
+        #endregion
+
+        #region Properties
+        public Collection<Reservation> AllReservations
+        {
+            get
+            {
+                return Reservations;
+            }
+        }
+        #endregion
+
+        #region Constructor
+        public ReservationController()
+        {
+            //***instantiate the ReservationDB object to communicate with the database
+            ReservationDB = new ReservationDB();
+            Reservations = ReservationDB.AllReservations;
+        }
+        #endregion
+
+        #region Database Communication.
+        public void DataMaintenance(Reservation aReservations, DB.DBOperation operation)
+        {
+            int index = 0;
+
+            switch (operation)
+            {
+                case DB.DBOperation.Add:
+                    Reservations.Add(aReservations);
+                    break;
+                case DB.DBOperation.Edit:
+                    index = FindIndex(aReservations);
+                    Reservations[index] = aReservations;
+                    break;
+                case DB.DBOperation.Delete:
+                    Reservations.Remove(aReservations);
+                    break;
+            }
+        }
+
+        public bool FinalizeChanges(Reservation Reservation)
+        {
+            return ReservationDB.UpdateDataSource(Reservation);
+        }
+
+        #endregion
+
+        #region Search Method
+
+
+        public Reservation Find(String ID)
+        {
+            int index = 0;
+            bool found = (Reservations[index].ReservationID == ID);
+            int count = Reservations.Count;
+            while (!(found) && (index < Reservations.Count - 1))
+            {
+                index = index + 1;
+                found = (Reservations[index].ReservationID == ID);
+            }
+            return Reservations[index];
+        }
+
+        public int FindIndex(Reservation aReservations)
+        {
+            int counter = 0;
+            bool found = false;
+            found = (aReservations.ReservationID == Reservations[counter].ReservationID);
+            while (found == false && counter != Reservations.Count - 1)
+            {
+                counter++;
+                found = (aReservations.ReservationID == Reservations[counter].ReservationID);
+            }
+            if (found)
+            {
+                return counter;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+        #endregion
+
     }
 }
--- Data/ReservationDB.cs
+++ Data/ReservationDB.cs
@@ -1,12 +1,258 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using INFOsProject.Business;
 
 namespace INFOsProject.Data
 {
-    internal class ReservationDB
+    public class ReservationDB:DB
     {
+        #region  Data members        
+        private string table = "Rersevation";
+        private string sqlLocal1 = "SELECT * FROM Rersevation";
+
+        private Collection<Reservation> reservations;
+
+        #endregion
+
+        #region Property Method: Collection
+        public Collection<Reservation> AllEmployees
+        {
+            get
+            {
+                return reservations;
+            }
+        }
+        #endregion
+
+        #region Constructor
+        public ReservationDB() : base()
+        {
+            reservations = new Collection<Reservation>();
+            FillDataSet(sqlLocal1, table);
+            Add2Collection(table);
+
+        }
+        #endregion
+
+        #region Utility Methods
+        public DataSet GetDataSet()
+        {
+            return dsMain;
+        }
+        private void Add2Collection(string table)
+        {
+            //Declare references to a myRow object and an Reservation object
+            DataRow myRow = null;
+            Reservation anEmp;
+            HeadWaiter headw;
+            Waiter waiter;
+            Runner runner;
+            Role.RoleType roleValue = Role.RoleType.NoRole;  //Declare roleValue and initialise
+            switch (table)
+            {
+                case "HeadWaiter":
+                    roleValue = Role.RoleType.Headwaiter;
+                    break;
+                case "Waiter":
+                    roleValue = Role.RoleType.Waiter;
+                    break;
+                case "Runner":
+                    roleValue = Role.RoleType.Runner;
+                    break;
+            }
+            //READ from the table  
+            foreach (DataRow myRow_loopVariable in dsMain.Tables[table].Rows)
+            {
+                myRow = myRow_loopVariable;
+                if (!(myRow.RowState == DataRowState.Deleted))
+                {
+                    //Instantiate a new Reservation object
+                    anEmp = new Reservation(roleValue);
+                    //Obtain each reservation attribute from the specific field in the row in the table
+                    anEmp.ID = Convert.ToString(myRow["ID"]).TrimEnd();
+                    //Do the same for all other attributes
+                    anEmp.EmployeeID = Convert.ToString(myRow["EmpID"]).TrimEnd();
+                    anEmp.Name = Convert.ToString(myRow["Name"]).TrimEnd();
+                    anEmp.Telephone = Convert.ToString(myRow["Phone"]).TrimEnd();
+                    anEmp.role.getRoleValue = (Role.RoleType)Convert.ToByte(myRow["Role"]);
+                    //Depending on Role read more Values
+                    switch (anEmp.role.getRoleValue)
+                    {
+                        case Role.RoleType.Headwaiter:
+                            headw = (HeadWaiter)anEmp.role;
+                            headw.SalaryAmount = Convert.ToDecimal(myRow["Salary"]);
+                            break;
+                        case Role.RoleType.Waiter:
+                            waiter = (Waiter)anEmp.role;
+                            waiter.getRate = Convert.ToDecimal(myRow["DayRate"]);
+                            waiter.getShifts = Convert.ToInt32(myRow["NoOfShifts"]);
+                            //was not included so added myslef
+                            waiter.getTips = Convert.ToDecimal(myRow["Tips"]);
+                            break;
+                        case Role.RoleType.Runner:
+                            runner = (Runner)anEmp.role;
+                            runner.getRate = Convert.ToDecimal(myRow["DayRate"]);
+                            runner.getShifts = Convert.ToInt32(myRow["NoOfShifts"]);
+                            // was not included so added myslef
+                            runner.getTips = Convert.ToDecimal(myRow["Tips"]);
+                            break;
+                    }
+                    reservations.Add(anEmp);
+                }
+            }
+        }
+        private void FillRow(DataRow aRow, Reservation anEmp)
+        {
+            HeadWaiter headwaiter;
+            Runner runner;
+            Waiter waiter;
+            aRow["ID"] = anEmp.ID;  //NOTE square brackets to indicate index of collections of fields in row.
+            aRow["EmpID"] = anEmp.EmployeeID;
+            aRow["Name"] = anEmp.Name;
+            aRow["Phone"] = anEmp.Telephone;
+            aRow["Role"] = (byte)anEmp.role.getRoleValue;
+            //*** For each role add the specific data variables
+            switch (anEmp.role.getRoleValue)
+            {
+                case Role.RoleType.Headwaiter:
+                    headwaiter = (HeadWaiter)anEmp.role;
+                    aRow["Salary"] = headwaiter.SalaryAmount;
+                    break;
+                case Role.RoleType.Waiter:
+                    waiter = (Waiter)anEmp.role;
+                    aRow["DayRate"] = waiter.getRate;
+                    aRow["NoOfShifts"] = waiter.getShifts;
+                    aRow["Tips"] = waiter.getTips;
+                    break;
+                case Role.RoleType.Runner:
+                    runner = (Runner)anEmp.role;
+                    aRow["DayRate"] = runner.getRate;
+                    aRow["NoOfShifts"] = runner.getShifts;
+                    aRow["Tips"] = runner.getTips;
+                    break;
+            }
+        }
+        #endregion
+
+        #region Database Operations CRUD
+        public void DataSetChange(Reservation anEmp)
+        {
+            DataRow aRow = null;
+            string dataTable = table;
+            switch (anEmp.role.getRoleValue)
+            {
+                case Role.RoleType.Headwaiter:
+                    dataTable = table;
+                    // aRow = dsMain.Tables[dataTable].NewRow();
+                    // FillRow(aRow, anEmp);
+                    // //Add to the dataset
+                    //dsMain.Tables[dataTable].Rows.Add(aRow);
+                    break;
+                case Role.RoleType.Waiter:
+                    dataTable = table2;
+                    break;
+                case Role.RoleType.Runner:
+                    dataTable = table3;
+                    break;
+            }
+            aRow = dsMain.Tables[dataTable].NewRow();
+            FillRow(aRow, anEmp);
+            //Add to the dataset
+            dsMain.Tables[dataTable].Rows.Add(aRow);
+        }
+        #endregion
+
+        #region Build Parameters, Create Commands & Update database
+        private void Build_INSERT_Parameters(Reservation anEmp)
+        {
+            //Create Parameters to communicate with SQL INSERT...add the input parameter and set its properties.
+            SqlParameter param = default(SqlParameter);
+            param = new SqlParameter("@ID", SqlDbType.NVarChar, 15, "ID");
+            daMain.InsertCommand.Parameters.Add(param);//Add the parameter to the Parameters collection.
+
+            param = new SqlParameter("@EMPID", SqlDbType.NVarChar, 10, "EMPID");
+            daMain.InsertCommand.Parameters.Add(param);
+
+            //Do the same for Description & answer -ensure that you choose the right size
+            param = new SqlParameter("@Name", SqlDbType.NVarChar, 100, "Name");
+            daMain.InsertCommand.Parameters.Add(param);
+
+            param = new SqlParameter("@Phone", SqlDbType.NVarChar, 15, "Phone");
+            daMain.InsertCommand.Parameters.Add(param);
+
+            param = new SqlParameter("@Role", SqlDbType.TinyInt, 1, "Role");
+            daMain.InsertCommand.Parameters.Add(param);
+            switch (anEmp.role.getRoleValue)
+            {
+                case Role.RoleType.Headwaiter:
+                    param = new SqlParameter("@Salary", SqlDbType.Money, 8, "Salary");
+                    daMain.InsertCommand.Parameters.Add(param);
+                    break;
+                case Role.RoleType.Waiter:
+                    param = new SqlParameter("@Tips", SqlDbType.Money, 8, "Tips");
+                    daMain.InsertCommand.Parameters.Add(param);
+
+                    param = new SqlParameter("@DayRate", SqlDbType.Money, 8, "DayRate");
+                    daMain.InsertCommand.Parameters.Add(param);
+
+                    param = new SqlParameter("@NoOfShifts", SqlDbType.SmallInt, 4, "NoOfShifts");
+                    daMain.InsertCommand.Parameters.Add(param);
+                    break;
+                case Role.RoleType.Runner:
+                    param = new SqlParameter("@DayRate", SqlDbType.Money, 8, "DayRate");
+                    daMain.InsertCommand.Parameters.Add(param);
+
+                    param = new SqlParameter("@NoOfShifts", SqlDbType.SmallInt, 4, "NoOfShifts");
+                    daMain.InsertCommand.Parameters.Add(param);
+                    break;
+            }
+            //***https://msdn.microsoft.com/en-za/library/ms179882.aspx
+        }
+
+        private void Create_INSERT_Command(Reservation anEmp)
+        {
+            //Create the command that must be used to insert values into the Books table..
+            switch (anEmp.role.getRoleValue)
+            {
+                case Role.RoleType.Headwaiter:
+                    daMain.InsertCommand = new SqlCommand("INSERT into HeadWaiter (ID, EMPID, Name, Phone, Role, Salary) VALUES (@ID, @EmpID, @Name, @Phone, @Role, @Salary)", cnMain);
+                    break;
+                case Role.RoleType.Waiter:
+                    daMain.InsertCommand = new SqlCommand("INSERT into Waiter (ID, EMPID, Name, Phone, Role, Tips, DayRate, NoOfShifts) VALUES (@ID, @EmpID, @Name, @Phone, @Role, @Tips, @DayRate, @NoOfShifts)", cnMain);
+                    break;
+                case Role.RoleType.Runner:
+                    daMain.InsertCommand = new SqlCommand("INSERT into Runner (ID, EMPID, Name, Phone, Role, DayRate, NoOfShifts) VALUES (@ID, @EmpID, @Name, @Phone, @Role, @DayRate, @NoOfShifts)", cnMain);
+                    break;
+            }
+            Build_INSERT_Parameters(anEmp);
+        }
+
+        public bool UpdateDataSource(Reservation anEmp)
+        {
+            bool success = true;
+            Create_INSERT_Command(anEmp);
+            switch (anEmp.role.getRoleValue)
+            {
+                case Role.RoleType.Headwaiter:
+                    success = UpdateDataSource(sqlLocal1, table);
+                    break;
+                case Role.RoleType.Waiter:
+                    success = UpdateDataSource(sqlLocal2, table2);
+                    break;
+                case Role.RoleType.Runner:
+                    success = UpdateDataSource(sqlLocal3, table3);
+                    break;
+            }
+            return success;
+        }
+
+        #endregion
+
     }
 }
