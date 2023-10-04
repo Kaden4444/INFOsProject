6a199ba2be999622e54e8a126ec536c89970a6bf
Dzivha Mushanganyisi <dzivha114@gmail.com>
2023-10-04 10:33:18 +0200

    Check comment below namespace

--- Presentation/MainUI.cs
+++ Presentation/MainUI.cs
@@ -11,6 +11,131 @@ using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace INFOsProject.Presentation
 {
+
+    /*
+        #region Events
+        private void EmployeeListingForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            listFormClosed = true;//4.4 Set the Boolean value of listFormClosed to true in the FormClosed event of the form(first create a FormClosed event for the form).
+
+        }
+        private void EmployeeListingForm_Load(object sender, EventArgs e)
+        {
+            employeeListView.View = View.Details;
+        }
+
+        private void EmployeeListingForm_Activated(object sender, EventArgs e)
+        {
+            // 4.3.1 Set the view of the employeeListView to Details view
+            employeeListView.View = View.Details;
+
+            // 4.3.2 Call the setUpEmployeeListView method
+            setUpEmployeeListView();
+
+            // 4.3.3 Call the ShowAll method to reset the controls
+            ShowAll(false, roleValue);
+        }
+        #endregion
+
+        #region List View
+        public void setUpEmployeeListView()
+        {
+            ListViewItem employeeDetails;   //Declare variables
+            HeadWaiter headW;
+            Waiter waiter;
+            Runner runner;
+            //Clear current List View Control
+            employeeListView.Clear();
+            //Set Up Columns of List View
+            employeeListView.Columns.Insert(0, "ID", 120, HorizontalAlignment.Left);
+            employeeListView.Columns.Insert(1, "EMPID", 120, HorizontalAlignment.Left);
+            employeeListView.Columns.Insert(2, "Name", 150, HorizontalAlignment.Left);
+            employeeListView.Columns.Insert(3, "Phone", 100, HorizontalAlignment.Left);
+            //TO DO  … do this for the other generic elements
+            employees = null;                      //employees collection will be filled by role
+            switch (roleValue)                     //  Check which role to add specific headings
+            {
+                case Role.RoleType.NoRole:
+                    // TO DO Get all the employees from the EmployeeController object 
+                    // (use the property) and assign to a local employees collection reference
+                    employees = employeeController.AllEmployees;
+                    listLabel.Text = "Listing of all employees";
+                    employeeListView.Columns.Insert(4, "Payment", 100, HorizontalAlignment.Center);
+                    break;
+                case Role.RoleType.Headwaiter:
+                    //Add a FindByRole method to the EmployeeController 
+                    employees = employeeController.FindByRole(employeeController.AllEmployees, Role.RoleType.Headwaiter);
+                    listLabel.Text = "Listing of all Headwaiters";
+                    //Set Up Columns of List View
+                    employeeListView.Columns.Insert(4, "Salary", 100, HorizontalAlignment.Center);
+                    break;
+                //do for the others
+                case Role.RoleType.Waiter:
+                    //Add a FindByRole method to the EmployeeController 
+                    employees = employeeController.FindByRole(employeeController.AllEmployees, Role.RoleType.Waiter);
+                    listLabel.Text = "Listing of all Waiters";
+                    //Set Up Columns of List View
+                    employeeListView.Columns.Insert(4, "Rate", 100, HorizontalAlignment.Center);
+                    employeeListView.Columns.Insert(4, "Number of Shifts", 100, HorizontalAlignment.Center);
+                    break;
+                case Role.RoleType.Runner:
+                    //Add a FindByRole method to the EmployeeController 
+                    employees = employeeController.FindByRole(employeeController.AllEmployees, Role.RoleType.Runner);
+                    listLabel.Text = "Listing of all Runners";
+                    //Set Up Columns of List View
+                    employeeListView.Columns.Insert(4, "Rate", 100, HorizontalAlignment.Center);
+                    employeeListView.Columns.Insert(4, "Number of Shifts", 100, HorizontalAlignment.Center);
+                    break;
+
+            }
+            //Add employee details to each ListView item 
+            foreach (Employee employee in employees)
+            {
+                employeeDetails = new ListViewItem();
+                employeeDetails.Text = employee.ID.ToString();
+                // Do the same for EmpID, Name and Phone
+                employeeDetails.SubItems.Add(employee.EmployeeID);
+                employeeDetails.SubItems.Add(employee.Name);
+                employeeDetails.SubItems.Add(employee.Telephone);
+
+                switch (employee.role.getRoleValue)
+                {
+                    case Role.RoleType.Headwaiter:
+                        headW = (HeadWaiter)employee.role;
+                        employeeDetails.SubItems.Add(headW.SalaryAmount.ToString());
+                        break;
+                    case Role.RoleType.Waiter:
+                        waiter = (Waiter)employee.role;
+                        employeeDetails.SubItems.Add(waiter.getRate.ToString());
+                        employeeDetails.SubItems.Add(waiter.getShifts.ToString());
+                        break;
+                    case Role.RoleType.Runner:
+                        runner = (Runner)employee.role;
+                        employeeDetails.SubItems.Add(runner.getRate.ToString());
+                        employeeDetails.SubItems.Add(runner.getShifts.ToString());
+                        break;
+                }
+                employeeListView.Items.Add(employeeDetails);
+            }
+            employeeListView.Refresh();
+            employeeListView.GridLines = true;
+        }
+
+
+        private void employeeListView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ShowAll(true, roleValue);
+            state = FormStates.View;
+            EnableEntries(false);
+            if (employeeListView.SelectedItems.Count > 0)   // if you selected an item 
+            {
+                employee = employeeController.Find(employeeListView.SelectedItems[0].Text);  //selected student becoms current student
+                                                                                             // Show the details of the selected student in the controls
+                PopulateTextBoxes(employee);
+            }
+        }
+        #endregion
+     */
     public partial class MainUI : Form
     {
         Dash d;
@@ -249,6 +374,11 @@ namespace INFOsProject.Presentation
         }
 
         private void ReservationPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void listView2_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
